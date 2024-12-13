73301e1c193775465ad4f3251b2bfe506c984592
braden <leachbr787@northwested.org>
2024-12-13 13:40:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,52 @@
+#
+#
+#
 
+
+#---Password Checker---#
+password = input("type your password: ")
+score = 0
+
+#Check the password for specific characters
+lowercase = False
+uppercase = False
+number = False
+punctuation = False
+for charcter in password: 
+    if charcter in 'abcdefghijklmnopqrstuvwxyz':
+        lowercase = True 
+    elif charcter in 'ABCDEFGHIJKLNOPQRSTUVWXYZ':
+        uppercase = True
+    elif charcter in '0123456789':
+        number = True 
+    else: 
+        punctuation = True 
+
+if lowercase == True:
+    print('Your password contains at least one lowercase character.')
+if uppercase == True: 
+   print(' Your password contains at least one uppercase character.')
+if number == True: 
+   print(' Your password contains at least one number character.')
+if punctuation == True: 
+   print(' Your password contains at least one punctuation.')
+
+
+
+if lowercase == True and uppercase == True: 
+    score = score + 10
+
+if number == True and (lowercase== True or uppercase==True): 
+    score += 10
+
+if punctuation == True:
+    score += 5
+
+if len(password) >= 8: 
+    score += 5
+    print('Your password is atleast 8 characters long.')
+
+
+
+
+print(f'\nScore: {str(score)}')
